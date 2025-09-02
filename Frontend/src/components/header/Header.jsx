17e340ba52f96a13867b2d8c03278dd6e4dd2d51
ajeetkumarnagla123 <149362237+ajeetkumarnagla123@users.noreply.github.com>
2025-09-02 16:10:40 +0530

    navbar update

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -96,31 +96,11 @@ const Header = () => {
 
             {/* Right Side Icons */}
             <div className="d-flex m-3 me-0">
-              <button
-                className="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4"
-                data-bs-toggle="modal"
-                data-bs-target="#searchModal"
-              >
-                <i className="fas fa-search text-primary"></i>
-              </button>
-
-              <Link to="/cart" className="position-relative me-4 my-auto">
-                <i className="fa fa-shopping-bag fa-2x"></i>
-                <span
-                  className="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1"
-                  style={{
-                    top: "-5px",
-                    left: "15px",
-                    height: "20px",
-                    minWidth: "20px",
-                  }}
-                >
-                  3
-                </span>
-              </Link>
+              
+              
 
               <Link to="/profile" className="my-auto">
-                <i className="fas fa-user fa-2x"></i>
+                Login
               </Link>
             </div>
           </div>

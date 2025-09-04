1ddd77cbb0269daedf3b4725b7e18d47e662cf06
ajeetkumarnagla123 <149362237+ajeetkumarnagla123@users.noreply.github.com>
2025-09-04 15:11:54 +0530

    update

--- Frontend/src/components/event/Eventshop.jsx
+++ Frontend/src/components/event/Eventshop.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../../components/event/eventshop.css";
 
 const Eventshop = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-2 shop_card">
-            <h1>birthday</h1>
+      <div className="container my-5">
+        <div className="row event_boxcard text-center g-3">
+          <div className="col-lg-2 col-md-4 col-sm-6 col-12">
+            <Link to="/birthday" className="shop_card birthday">
+              <h2>Birthday</h2>
+            </Link>
+          </div>
+
+          <div className="col-lg-2 col-md-4 col-sm-6 col-12">
+            <Link to="/wedding" className="shop_card wedding">
+              <h2>Wedding</h2>
+            </Link>
+          </div>
+
+          <div className="col-lg-2 col-md-4 col-sm-6 col-12">
+            <Link to="/concert" className="shop_card concert">
+              <h2>Concert</h2>
+            </Link>
+          </div>
+
+          <div className="col-lg-2 col-md-4 col-sm-6 col-12">
+            <Link to="/party" className="shop_card party">
+              <h2>Party</h2>
+            </Link>
+          </div>
+
+          <div className="col-lg-2 col-md-4 col-sm-6 col-12">
+            <Link to="/festival" className="shop_card festival">
+              <h2>Festival</h2>
+            </Link>
           </div>
         </div>
       </div>

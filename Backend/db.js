38ad01edf0e305cea1ccd795be677ba6ed277f55
ajeetkumarnagla123 <149362237+ajeetkumarnagla123@users.noreply.github.com>
2025-09-10 15:55:48 +0530

    update

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2");
+import '../Backend/.env'
 
 const db = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "Shubham@12",
+  password: "Shubham@12" || DB_PASSWORD,
   database: "event_management", // apna DB name
   port: 3306,
 });

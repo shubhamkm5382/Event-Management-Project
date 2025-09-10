c7564780e485a75d51ee316e7a101da172a61571
Shubham Kumar <shubhamkm5382@gmail.com>
2025-09-10 13:48:58 +0530

    Update media route to include 'gallery' prefix and adjust fetch call in GalleryPage for correct API endpoint

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-
-=======
 // import '@fortawesome/fontawesome-free/css/all.min.css'
->>>>>>> 90ba89cb9c6a6957d64ee7e4ce10b877abdae3d0
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter } from 'react-router-dom'

821dfb8390433f5a2554e1473dd837ecfd8d0bd5
ajeetkumarnagla123 <149362237+ajeetkumarnagla123@users.noreply.github.com>
2025-09-15 12:26:52 +0530

    update

--- Frontend/src/components/GalleryPage/CategoryTabs/CategoryTabs.jsx
+++ Frontend/src/components/GalleryPage/CategoryTabs/CategoryTabs.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./CategoryTabs.module.css";
 
-export default function CategoryTabs({ categories, activeCategory, setActiveCategory }) {
+export default function CategoryTabs({
+  categories,
+  activeCategory,
+  setActiveCategory,
+}) {
   return (
     <div className={styles["filter-bar"]}>
       {categories.map((cat) => (
         <button
           key={cat}
-          className={`${styles["filter-btn"]} ${activeCategory === cat ? styles.active : ""}`}
+          className={`${styles["filter-btn"]} ${
+            activeCategory === cat ? styles.active : ""
+          }`}
           onClick={() => setActiveCategory(cat)}
         >
           {cat.charAt(0).toUpperCase() + cat.slice(1)}

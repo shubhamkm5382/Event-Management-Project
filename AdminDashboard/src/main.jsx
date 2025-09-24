b819dc0aedd9f109723a7cffb4fae449ce4659f4
Shubham Kumar <shubhamkm5382@gmail.com>
2025-09-24 16:58:15 +0530

    update

--- AdminDashboard/src/main.jsx
+++ AdminDashboard/src/main.jsx
@@ -9,15 +9,10 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< HEAD
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </ClerkProvider>
-  </StrictMode>,
-=======
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
   </StrictMode>
->>>>>>> bfbdd51c13b469fdc2a8eb298d55bbdbfea18fd0
 )

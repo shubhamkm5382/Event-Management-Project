67e62985b54010345bddd27d85fd12d826cd4ad9
Shubham Kumar <shubhamkm5382@gmail.com>
2025-09-24 09:51:30 +0530

    Minor update

--- AdminDashboard/src/App.jsx
+++ AdminDashboard/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-// import EnhancedAdminDashboard from './EnhancedAdminDashboard';
+
 import AdminDashboard from './AdminDashboard'
 
 function App() {
   return (
     <div className="App">
-      {/* <EnhancedAdminDashboard /> */}
       <AdminDashboard/>
     </div>
   );
